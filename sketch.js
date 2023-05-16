e286f1b67e6c8ad23a382aaaafd5a596c8d17deb
wagnergabanella23 <132388574+wagnergabanella23@users.noreply.github.com>
2023-05-16 14:54:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+
+var wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12
+
+var count
+
+
+function setup() {
+  createCanvas(400, 400); 
+  wall1 = createSprite(190, 120,250,  3)
+  // wall2 = createSprite(190, 260, 250, 3)
+  // wall3 = createSprite(67, 145, 3, 50)
+  // wall4 = createSprite(64, 235, 3, 50)
+  // wall5 = createSprite(313, 145, 3, 50)
+  // wall6 = createSprite(313, 235, 3, 50)
+  //  wall7 = createSprite(41, 170, 50, 3)
+  //  wall8 = createSprite(41, 210, 50, 3)
+  //  wall9 = createSprite(337, 210, 50, 3)
+  //  wall10 = createSprite(337, 170, 50, 3)
+  //  wall11 = createSprite(18, 190, 3, 40)
+  //  wall12 = createSprite(361, 190, 3, 40)
+
+
+
+
+ 
+  
+
+  
+  
+
+  
+}
+
+
+
+
+
+function draw() {
+  background('#b9bada');
+
+  
+    
+    
+    
+    
+  drawSprites()
+}
